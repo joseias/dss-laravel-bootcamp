edd0b9663aba8497fc9dbac03b6864ef528c6969
joseias <joseias@gmail.com>
2025-04-08 13:55:00 +0000

    refactoring

--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,15 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+
+// apply auth middleware to profile relate routes
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+// create standard CRUD routes
 Route::resource(name: 'chirps', controller: ChirpController::class)
     ->only(['index', 'store', 'edit', 'update', 'destroy'])
     ->middleware(middleware: ['auth', 'verified']);
